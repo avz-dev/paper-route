818fc470ef77a5c8bf0b579f0811985c7e96cec7
Andrew Valdez <avz@unm.edu>
2023-01-28 15:32:31 -0700

    added background parallax

---
+++ Assets/Scripts/Parallax/Parallax.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+    [SerializeField] private float speed;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        meshRenderer.material.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0);
+    }
+}
